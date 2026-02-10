4c4eef21b52ea42f9a76a3179ee4e61e4d64f199
uyas2207 <uyas2207@mail.ru>
2026-02-10 13:57:49 +0300

    before client rework

--- client/startClient.js
+++ client/startClient.js
@@ -44,16 +44,7 @@ class PatrolClient {
 
         this.init();
     }
-         /*           
-        alt.log("=== ВСЁ О PED ===");
-        for (let key in entity) {
-            try {
-                alt.log(`${key} = ${entity[key]}`);
-            } catch (error) {
-                // нужно что бы код продолжил выполняться после ошибки если она будет
-            }
-        }
-        */
+    
     init(){
         
 
@@ -92,7 +83,7 @@ class PatrolClient {
             if(!(entity instanceof alt.Ped)) return;
             alt.log('entity.scriptID', entity.scriptID);
 
-            // при повторном появлении ped на клиенте, меняется scriptID и другие значения 
+            // при повторном появлении ped на клиенте, меняется scriptID и другие значения, но остается тем же id
             if (this.currentPed.has(entity.id)) {
                 const data = this.currentPed.get(entity.id);
                 data.entity = entity; //изменение значений для ped с id
@@ -104,7 +95,7 @@ class PatrolClient {
                 }
                 return;
             }
-            //при первом появлении ped на клиенте 
+            //при первом появлении ped на клиенте
             this.currentPed.set(entity.id, {
                 entity,
                 asignedRoute: null,
@@ -446,19 +437,7 @@ drawPedMarkers(){
 
 }
 
-drawRouteMarkers(routeMap) {
-    routeMap.forEach((point) => {
-        native.drawMarker(
-            this.defaultConfig.markerType + point.index,
-            point.position.x, point.position.y, point.position.z,
-            0, 0, 0,
-            0, 0, 0,
-            this.defaultConfig.markerScale.x, this.defaultConfig.markerScale.y, this.defaultConfig.markerScale.z,
-            this.defaultConfig.markerColour.r, this.defaultConfig.markerColour.g, this.defaultConfig.markerColour.b, this.defaultConfig.markerColour.a,
-            false, true, 2, 0, 0, 0, false
-        );
-    });
-}
+
 //отображение линий между маркерами (показывает от какого маркера к какому будет ходить ped)
 connectAllRoutesLine() {
     this.mainMap.forEach(({ nodes, attributes }) => {
@@ -468,129 +447,12 @@ connectAllRoutesLine() {
     });
 }
 
-connectNodesLine(routeMap, attributes){
-    if (routeMap.size < 2) return;
- 
-    const first = routeMap.values().next().value;
-    let prev = null;
 
-    routeMap.forEach((current) => {
-        if (current !== first) {
-            native.drawLine(
-                prev.position.x,
-                prev.position.y,
-                prev.position.z,
-                current.position.x,
-                current.position.y,
-                current.position.z,
-                this.defaultConfig.markerColour.r,
-                this.defaultConfig.markerColour.g,
-                this.defaultConfig.markerColour.b,
-                this.defaultConfig.markerColour.a
-            );
-        }
-        prev = current;
-    });
-
-    if (attributes.looped) {
-        native.drawLine(
-            prev.position.x,
-            prev.position.y,
-            prev.position.z,
-            first.position.x,
-            first.position.y,
-            first.position.z,
-            this.defaultConfig.markerColour.r,
-            this.defaultConfig.markerColour.g,
-            this.defaultConfig.markerColour.b,
-            this.defaultConfig.markerColour.a
-        );
-    }
-}
 
     //const coneColor = cansee ? { r: 255, g: 0, b: 0, a: 200 } : { r: 0, g: 255, b: 0, a: 200 };
-//отображение области видимости ped
-drawPedVisionCone(pedPos, pedScriptID, playerpos) {
-    const heading = native.getEntityHeading(pedScriptID);
-
-    const headingRad = heading * Math.PI / 180;
-    const halfAngleRad = (this.viewAngle / 2) * Math.PI / 180;
-    const stepAngleRad = (this.viewAngle * Math.PI / 180) / this.viewSectors;
-
-    const cansee = this.isPlayerInVisionCone(playerpos, headingRad, halfAngleRad, pedPos);
-    let prevPoint = null;
-    let coneColor = { r: 0, g: 255, b: 0, a: 200 };
-
-    if (cansee){
-        coneColor = { r: 255, g: 0, b: 0, a: 200 };
-        native.drawMarker(
-            0,
-            playerpos.x, playerpos.y, playerpos.z + 1.0,
-            0, 0, 0,
-            0, 0, 0,
-            0.15, 0.15, 0.15,
-            255, 0, 0, 200,
-            true, true, 2, 0, 0, 0, false
-        );
-        native.drawLine(
-            pedPos.x, pedPos.y, pedPos.z + 0.1,
-            playerpos.x, playerpos.y, playerpos.z + 0.5,
-            255, 0, 0, 200
-        );
-    }
-    for (let i = -halfAngleRad; i <= halfAngleRad; i += stepAngleRad) {
-        const currentAngle = headingRad + i;
-
-        const forwardX = Math.sin(-currentAngle);
-        const forwardY = Math.cos(-currentAngle);
-
-        const x = pedPos.x + forwardX * this.viewDistance;
-        const y = pedPos.y + forwardY * this.viewDistance;
-        const z = pedPos.z;
-
-        native.drawLine(
-            pedPos.x, pedPos.y, pedPos.z + 0.1,
-            x, y, z + 0.1,
-            coneColor.r, coneColor.g, coneColor.b, coneColor.a
-        );
-
-        if (prevPoint) {
-            native.drawLine(
-                prevPoint.x, prevPoint.y, prevPoint.z + 0.1,
-                x, y, z + 0.1,
-                coneColor.r, coneColor.g, coneColor.b, coneColor.a
-            );
-        }
-
-        prevPoint = { x, y, z };
-    }
-}
-
-//            if (!this.isPlayerInSight && this.whichPedhasPlayerinVisionCone === pedScriptID) {
-
-isPlayerInVisionCone(playerPos, headingRad, halfAngleRad, pedPos) {
-
-    const toPlayerX = playerPos.x - pedPos.x;
-    const toPlayerY = playerPos.y - pedPos.y;
-
-    const distance = Math.sqrt(toPlayerX * toPlayerX + toPlayerY * toPlayerY);
-    if (distance > this.viewDistance) return false;
-
-    const forwardX = Math.sin(-headingRad);
-    const forwardY = Math.cos(-headingRad);
-
-    const len = Math.sqrt(toPlayerX * toPlayerX + toPlayerY * toPlayerY);
-    const dirToPlayerX = toPlayerX / len;
-    const dirToPlayerY = toPlayerY / len;
-
-    const dot = forwardX * dirToPlayerX + forwardY * dirToPlayerY;
-
-    const angleToPlayer = Math.acos(dot);
-
-    return angleToPlayer <= halfAngleRad;
-}
 
 
+/*
         async setPedClient(ped){
             alt.log(`setPedClient ${ped}`);
             await new Promise(resolve => alt.setTimeout(resolve, 500));
@@ -605,7 +467,7 @@ isPlayerInVisionCone(playerPos, headingRad, halfAngleRad, pedPos) {
             
             alt.log(`headingRad: ${headingRad}`);
         }
-
+*/
 //назначение маршрута ped
 async asignCurrentRouteToPed(ped, attributes, nodes) {
     if ( nodes.size === 0 ){
@@ -655,7 +517,146 @@ async asignCurrentRouteToPed(ped, attributes, nodes) {
 
 }
 
+class DebugVisuals{
+    constructor() {
 
+    }
+
+    drawRouteMarkers(routeMap) {
+        routeMap.forEach((point) => {
+            native.drawMarker(
+                this.defaultConfig.markerType + point.index,
+                point.position.x, point.position.y, point.position.z,
+                0, 0, 0,
+                0, 0, 0,
+                this.defaultConfig.markerScale.x, this.defaultConfig.markerScale.y, this.defaultConfig.markerScale.z,
+                this.defaultConfig.markerColour.r, this.defaultConfig.markerColour.g, this.defaultConfig.markerColour.b, this.defaultConfig.markerColour.a,
+                false, true, 2, 0, 0, 0, false
+            );
+        });
+    }
+
+    connectNodesLine(routeMap, attributes){
+        if (routeMap.size < 2) return;
+    
+        const first = routeMap.values().next().value;
+        let prev = null;
+
+        routeMap.forEach((current) => {
+            if (current !== first) {
+                native.drawLine(
+                    prev.position.x,
+                    prev.position.y,
+                    prev.position.z,
+                    current.position.x,
+                    current.position.y,
+                    current.position.z,
+                    this.defaultConfig.markerColour.r,
+                    this.defaultConfig.markerColour.g,
+                    this.defaultConfig.markerColour.b,
+                    this.defaultConfig.markerColour.a
+                );
+            }
+            prev = current;
+        });
+
+        if (attributes.looped) {
+            native.drawLine(
+                prev.position.x,
+                prev.position.y,
+                prev.position.z,
+                first.position.x,
+                first.position.y,
+                first.position.z,
+                this.defaultConfig.markerColour.r,
+                this.defaultConfig.markerColour.g,
+                this.defaultConfig.markerColour.b,
+                this.defaultConfig.markerColour.a
+            );
+        }
+    }
+
+    //отображение области видимости ped
+    drawPedVisionCone(pedPos, pedScriptID, playerpos) {
+        const heading = native.getEntityHeading(pedScriptID);
+
+        const headingRad = heading * Math.PI / 180;
+        const halfAngleRad = (this.viewAngle / 2) * Math.PI / 180;
+        const stepAngleRad = (this.viewAngle * Math.PI / 180) / this.viewSectors;
+
+        const cansee = this.isPlayerInVisionCone(playerpos, headingRad, halfAngleRad, pedPos);
+        let prevPoint = null;
+        let coneColor = { r: 0, g: 255, b: 0, a: 200 };
+
+        if (cansee){
+            coneColor = { r: 255, g: 0, b: 0, a: 200 };
+            native.drawMarker(
+                0,
+                playerpos.x, playerpos.y, playerpos.z + 1.0,
+                0, 0, 0,
+                0, 0, 0,
+                0.15, 0.15, 0.15,
+                255, 0, 0, 200,
+                true, true, 2, 0, 0, 0, false
+            );
+            native.drawLine(
+                pedPos.x, pedPos.y, pedPos.z + 0.1,
+                playerpos.x, playerpos.y, playerpos.z + 0.5,
+                255, 0, 0, 200
+            );
+        }
+        for (let i = -halfAngleRad; i <= halfAngleRad; i += stepAngleRad) {
+            const currentAngle = headingRad + i;
+
+            const forwardX = Math.sin(-currentAngle);
+            const forwardY = Math.cos(-currentAngle);
+
+            const x = pedPos.x + forwardX * this.viewDistance;
+            const y = pedPos.y + forwardY * this.viewDistance;
+            const z = pedPos.z;
+
+            native.drawLine(
+                pedPos.x, pedPos.y, pedPos.z + 0.1,
+                x, y, z + 0.1,
+                coneColor.r, coneColor.g, coneColor.b, coneColor.a
+            );
+
+            if (prevPoint) {
+                native.drawLine(
+                    prevPoint.x, prevPoint.y, prevPoint.z + 0.1,
+                    x, y, z + 0.1,
+                    coneColor.r, coneColor.g, coneColor.b, coneColor.a
+                );
+            }
+
+            prevPoint = { x, y, z };
+        }
+    }
+
+//            if (!this.isPlayerInSight && this.whichPedhasPlayerinVisionCone === pedScriptID) {
+
+    isPlayerInVisionCone(playerPos, headingRad, halfAngleRad, pedPos) {
+
+        const toPlayerX = playerPos.x - pedPos.x;
+        const toPlayerY = playerPos.y - pedPos.y;
+
+        const distance = Math.sqrt(toPlayerX * toPlayerX + toPlayerY * toPlayerY);
+        if (distance > this.viewDistance) return false;
+
+        const forwardX = Math.sin(-headingRad);
+        const forwardY = Math.cos(-headingRad);
+
+        const len = Math.sqrt(toPlayerX * toPlayerX + toPlayerY * toPlayerY);
+        const dirToPlayerX = toPlayerX / len;
+        const dirToPlayerY = toPlayerY / len;
+
+        const dot = forwardX * dirToPlayerX + forwardY * dirToPlayerY;
+
+        const angleToPlayer = Math.acos(dot);
+
+        return angleToPlayer <= halfAngleRad;
+    }
+}
 
 new PatrolClient();
 
